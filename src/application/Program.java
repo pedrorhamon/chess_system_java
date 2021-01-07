d11aeb8cea6136e2ee2cfffefe25cfbedaa0c803
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-01-07 12:22:28 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Ola mundo!");
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
