c894352e49f5bf0b696c010144df242af03c1bcf
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-01-08 01:44:48 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-	
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -23,32 +27,42 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} 
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8. ");
+		}
+	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
-		
-		for (int i=0; i<pieces.length; i++) {
-			System.out.print((8-i) + " ");
-			for (int j=0; j<pieces.length; j++) {
+
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
-				
+
 			}
-			System.out.println( );
+			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	private static void printPiece(ChessPiece piece) {
-		if (piece==null) {
+		if (piece == null) {
 			System.out.print("-");
-		}
-		else {
+		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_BLUE + piece + ANSI_RESET);
 			}
-			else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);		
-	}
-}
-		System.out.println(" ");
+		}
+		System.out.print(" ");
 	}
 }
