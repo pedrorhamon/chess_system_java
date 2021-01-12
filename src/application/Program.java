14a1d9e237f224a16b2ccc0edc606be3ab91a54f
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-01-12 14:30:55 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				
+				if (chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
